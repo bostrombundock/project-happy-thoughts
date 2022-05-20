d797dca67c9c1fdc48022a67796f9757a6a70269
bostrombundock <sbostrom@ymail.com>
2022-05-20 20:06:16 +0200

    added new link

--- code/src/App.js
+++ code/src/App.js
@@ -17,7 +17,7 @@ export const App = () => {
 
   const fetchThought = () => {
     setLoading(true);
-    fetch(`https://appy-thoughts.herokuapp.com//thoughts`)
+    fetch('https://appy-thoughts.herokuapp.com/thoughts')
       .then((res) => res.json())
       .then((data) => setThought(data))
       .finally(() => setLoading(false))
@@ -35,7 +35,7 @@ export const App = () => {
     }
 //orginal url `https://happy-thoughts-technigo.herokuapp.com/thoughts`
 
-    fetch(`https://appy-thoughts.herokuapp.com/thoughts`, options)
+    fetch('https://appy-thoughts.herokuapp.com/thoughts', options)
       .then((res) => res.json())
       .then(() => {
         fetchThought(setNewThought(""))
@@ -51,7 +51,7 @@ export const App = () => {
       },
     }
 
-    fetch(`https://appy-thoughts.herokuapp.com/${thoughtId}/like`, options)
+    fetch('https://appy-thoughts.herokuapp.com/${thoughtId}/like', options)
       .then(res => res.json())
       .then(() => fetchThought())
     fetchThought()
